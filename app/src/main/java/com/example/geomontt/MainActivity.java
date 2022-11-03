cb1c55998e7c379450d8177e6551ed781be5da4b
Bunnyfifi <matias.oyarzun.curu@gmail.com>
2022-11-03 11:54:05 -0300

    agregamos clase Usuario y daoUsuario para la conexion con base de datos

--- app/src/main/java/com/example/geomontt/MainActivity.java
+++ app/src/main/java/com/example/geomontt/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+//EditText user, pass;
+//Button btnEntrar, btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        user=(EditText) findViewById(R.id.User);
     }
     public void menu(View view){
         Intent i = new Intent(this, Menu.class);
